efaafe78dec8d58c4f686839e2c5357c4b20fc9d
windyludev <windyludev@outlook.com>
2019-04-23 09:52:00 -0400

    Organize and refactor code

--- guessing_game.py
+++ guessing_game.py
@@ -1,60 +1,58 @@
-"""
-Python Web Development
-Project 1 - Number Guessing Game
---------------------------------
-"""
-import random
+# Number Guessing Game
 
-min_numb = 0
-max_numb = 20
-messages = ["Your number is bigger than max",
-            "It's lower",
-            "It's higher",
+import random
+min_number = 0
+max_number = 20
+messages = ["You got it! \U0001F3C6",
+            "Your number is larger than max",
+            "Your number's higher",
+            "Your number's lower",
             "Your number is smaller than minimun"]
 
-
 def start_game():
-    number_to_guess = random.randint(min_numb, max_numb)
-    try:
-        user_input = int(
-            input("Pick a number between {} and {}\n> ".format(min_numb, max_numb)))
-    except ValueError:
-        user_input = False
-
-    attemps = [user_input]
-
+    number_to_guess = random.randint(min_number, max_number)
+    user_input = None
+    attemps = []
+    
     while number_to_guess != user_input:
+        if len(attemps) == 0:
+            messages = "Pick a number between {} and {}\n> "
+        else:
+            messages = "\nTry another number:\n> "
+        user_input = get_user_guest(messages, number_to_guess)
         attemps.append(user_input)
-        display_message(user_input, number_to_guess)
-        try:
-            user_input = int(input("\nTry another number:\n> "))
-        except ValueError:
-            user_input = False
 
-    print("\u2714  You got it!\nIt took you: {} tries\n".format(len(attemps)))
+    print("It took you: {} tries\n".format(len(attemps)))
     return len(attemps)
 
-
-def display_message(number, pivot):
-    if not number:
-        print("\u274C  Your input is not a number, try again")
-    elif number > max_numb:
-        print(messages[0])
-    elif number > pivot:
-        print(messages[1])
-    elif number >= min_numb:
-        print(messages[2])
+def get_user_guest(message, number_to_guess):
+    try:
+        user_number = int(input(message.format(min_number, max_number)))
+    except ValueError:
+        print("Your input is not a number \u274C")
+        user_number = None
     else:
-        print(messages[3])
+        if user_number == number_to_guess:
+            print(messages[0])
+        elif user_number > max_number:
+            print(messages[1])
+        elif user_number > number_to_guess:
+            print(messages[2])
+        elif user_number >= min_number:
+            print(messages[3])
+        else:
+            print(messages[4])
+
+    return user_number
 
 def select_highscore(new_score, last_score):
     if new_score < last_score:
         return new_score
     else:
         return last_score
-    
+
 if __name__ == '__main__':
-    print("""\n--------------------------------------------\n \u2B50   Welcome to the Number Guesing Game! \u2B50\n--------------------------------------------\n""")
+    print("\n","-" * 43, "\n \u2B50   Welcome to the Number Guesing Game! \u2B50 \n", "-" * 43, "\n")
     score = start_game()
 
     while True:
@@ -62,9 +60,7 @@ if __name__ == '__main__':
         if answer.lower() == 'n':
             break
         elif answer.lower() != 'y':
-            print("Your input is not correct \U0001F4A9 \n")
+            print("Your input is not correct \u274C\n")
         else:
-            print("\n\n----------------------------\n \U0001F3C5   The HIGHSCORE is {} \U0001F3C5 \n----------------------------\n".format(score))
+            print("\n", "-" * 26, "\n \U0001F3C5   The HIGHSCORE is {} \U0001F3C5 \n".format(score), "-" * 26,"\n")
             score = select_highscore(start_game(), score)
-
-            
